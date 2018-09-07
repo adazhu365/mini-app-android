195b74922833bf9b3a831b35d21967938491c073
Siteng <sz4dd@virginia.edu>
2018-09-07 14:39:46 -0400

    first commit, tried Sheriff's neatapp code

--- app/src/main/java/edu/sitengvirginia/androidminiapp/MainActivity.java
+++ app/src/main/java/edu/sitengvirginia/androidminiapp/MainActivity.java
@@ -2,12 +2,25 @@ package edu.sitengvirginia.androidminiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView myText;
+    private Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myText = findViewById(R.id.textView);
+        myButton = findViewById(R.id.button);
+    }
+
+    public void changeText(View view) {
+        myText.setText("You clicked the button!");
     }
 }
