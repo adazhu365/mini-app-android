c3075531f6b2789477072aa906c2715a0908fc41
Siteng <sz4dd@virginia.edu>
2018-09-16 15:17:28 -0400

    changed fab into an actual button that displays right

--- app/src/main/java/edu/sitengvirginia/androidminiapp/AddItemActivity.java
+++ app/src/main/java/edu/sitengvirginia/androidminiapp/AddItemActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -18,8 +19,9 @@ public class AddItemActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button button = findViewById(R.id.button);
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "NOTHING HERE", Snackbar.LENGTH_LONG)
